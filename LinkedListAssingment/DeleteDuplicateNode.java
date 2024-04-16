ad6c488656ce406783ecd36e07c7b1a898b1adc0
Raushan Kumar <92353031+raushan736834@users.noreply.github.com>
2024-04-16 13:21:17 +0530

    Add files via upload

---
+++ LinkedListAssingment/DeleteDuplicateNode.java
@@ -0,0 +1,80 @@
+package LinkedListAssingment;
+
+class Node {
+    int data;
+    Node next;
+};
+
+class DeleteDuplicateNode {
+  static Node head;
+
+  DeleteDuplicateNode(){
+    head = null;
+  }
+
+  //Add new element at the end of the list
+  void push_back(int newElement) {
+    Node newNode = new Node();
+    newNode.data = newElement;
+    newNode.next = null; 
+    if(head == null) {
+      head = newNode;
+    } else {
+      Node temp = new Node();
+      temp = head;
+      while(temp.next != null)
+        temp = temp.next;
+      temp.next = newNode;
+    }    
+  }
+
+   static void deleteDuplicates() {
+        if(head == null || head.next == null)
+            return ;
+        Node curr = head;
+        
+        while( curr != null && curr.next != null){
+           
+            if(curr.data == curr.next.data){
+                curr.next = curr.next.next;
+            }
+            else{
+                curr = curr.next; 
+            }
+        }
+        return;
+    }
+  //display the content of the list
+  void PrintList() {
+    Node temp = new Node();
+    temp = DeleteDuplicateNode.head;
+    if(temp != null) {
+      System.out.print("The list contains: ");
+      while(temp != null) {
+        System.out.print(temp.data + " ");
+        temp = temp.next;
+      }
+      System.out.println();
+    } else {
+      System.out.println("The list is empty.");
+    }
+  }    
+
+  public static void main(String[] args) {
+    DeleteDuplicateNode MyList = new DeleteDuplicateNode();
+
+    //Add three elements at the end of the list.
+    MyList.push_back(3);
+    MyList.push_back(3);
+    MyList.push_back(5);
+    MyList.push_back(6);
+    MyList.push_back(6);
+    MyList.push_back(7);
+    MyList.PrintList();
+    
+    deleteDuplicates();
+    
+    MyList.PrintList();
+  
+  }
+}
